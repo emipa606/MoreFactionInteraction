2979741b0ea9ced450871435a6addea5afc52d09
emipa606 <emil.offermo@litet.net>
2021-02-13 22:02:14 +0100

    Increased the maximum value for time modifier for faction interaction to 10 by request

--- Source/MoreFactionInteraction/General/Settings.cs
+++ Source/MoreFactionInteraction/General/Settings.cs
@@ -1,8 +1,7 @@
-﻿using RimWorld;
+﻿using JetBrains.Annotations;
+using RimWorld;
 using UnityEngine;
 using Verse;
-using JetBrains.Annotations;
-
 
 namespace MoreFactionInteraction
 {
@@ -16,40 +15,49 @@ namespace MoreFactionInteraction
         public void DoWindowContents(Rect rect)
         {
             var options = new Listing_Standard();
-            options.Begin(rect: rect);
+            options.Begin(rect);
             options.Gap();
-            options.SliderLabeled(label: "MFI_ticksToUpgrade".Translate(), val: ref ticksToUpgrade, format: ticksToUpgrade.ToStringTicksToPeriodVague(vagueMin: false), min: 0, max: GenDate.TicksPerYear, "MFI_ticksToUpgradeDesc".Translate());
+            options.SliderLabeled("MFI_ticksToUpgrade".Translate(), ref ticksToUpgrade,
+                ticksToUpgrade.ToStringTicksToPeriodVague(false), 0, GenDate.TicksPerYear,
+                "MFI_ticksToUpgradeDesc".Translate());
             options.Gap();
-            options.SliderLabeled(label: "MFI_pirateBaseUpgraderModifier".Translate(), val: ref pirateBaseUpgraderModifier, format: pirateBaseUpgraderModifier.ToStringByStyle(style: ToStringStyle.FloatOne), min: 0.1f, max: 2f, "MFI_pirateBaseUpgraderModifierDesc".Translate());
+            options.SliderLabeled("MFI_pirateBaseUpgraderModifier".Translate(), ref pirateBaseUpgraderModifier,
+                pirateBaseUpgraderModifier.ToStringByStyle(ToStringStyle.FloatOne), 0.1f, 2f,
+                "MFI_pirateBaseUpgraderModifierDesc".Translate());
             options.GapLine();
-            options.SliderLabeled(label: "MFI_timeModifierBetweenFactionInteraction".Translate(), val: ref timeModifierBetweenFactionInteraction, format: timeModifierBetweenFactionInteraction.ToStringByStyle(style: ToStringStyle.FloatOne), min: 0.5f, max: 3f, "MFI_timeModifierBetweenFactionInteractionDesc".Translate());
+            options.SliderLabeled("MFI_timeModifierBetweenFactionInteraction".Translate(),
+                ref timeModifierBetweenFactionInteraction,
+                timeModifierBetweenFactionInteraction.ToStringByStyle(ToStringStyle.FloatOne), 0.5f, 10f,
+                "MFI_timeModifierBetweenFactionInteractionDesc".Translate());
             options.Gap();
-            options.SliderLabeled(label: "MFI_traderWealthOffsetFromTimesTraded".Translate(), val: ref traderWealthOffsetFromTimesTraded, format: traderWealthOffsetFromTimesTraded.ToStringByStyle(style: ToStringStyle.FloatOne), min: 0.5f, max: 3f);
+            options.SliderLabeled("MFI_traderWealthOffsetFromTimesTraded".Translate(),
+                ref traderWealthOffsetFromTimesTraded,
+                traderWealthOffsetFromTimesTraded.ToStringByStyle(ToStringStyle.FloatOne), 0.5f, 3f);
             options.End();
             Mod.GetSettings<MoreFactionInteraction_Settings>().Write();
         }
 
         public override void ExposeData()
         {
-            Scribe_Values.Look(value: ref ticksToUpgrade, label: "ticksToUpgrade", defaultValue: 2700000);
-            Scribe_Values.Look(value: ref timeModifierBetweenFactionInteraction, label: "timeModifierBetweenFactionInteraction", defaultValue: 1f);
-            Scribe_Values.Look(value: ref traderWealthOffsetFromTimesTraded, label: "traderWealthOffsetFromTimesTraded", defaultValue: 0.8f);
-            Scribe_Values.Look(value: ref pirateBaseUpgraderModifier, label: "MFI_pirateBaseUpgraderModifier", defaultValue: 0.8f);
+            Scribe_Values.Look(ref ticksToUpgrade, "ticksToUpgrade", 2700000);
+            Scribe_Values.Look(ref timeModifierBetweenFactionInteraction, "timeModifierBetweenFactionInteraction", 1f);
+            Scribe_Values.Look(ref traderWealthOffsetFromTimesTraded, "traderWealthOffsetFromTimesTraded", 0.8f);
+            Scribe_Values.Look(ref pirateBaseUpgraderModifier, "MFI_pirateBaseUpgraderModifier", 0.8f);
         }
     }
 
     [UsedImplicitly]
     public class MoreFactionInteractionMod : Mod
     {
-        public MoreFactionInteractionMod(ModContentPack content) : base(content: content)
+        public MoreFactionInteractionMod(ModContentPack content) : base(content)
         {
             GetSettings<MoreFactionInteraction_Settings>();
         }
 
         public override void DoSettingsWindowContents(Rect inRect)
         {
-            base.DoSettingsWindowContents(inRect: inRect);
-            GetSettings<MoreFactionInteraction_Settings>().DoWindowContents(rect: inRect);
+            base.DoSettingsWindowContents(inRect);
+            GetSettings<MoreFactionInteraction_Settings>().DoWindowContents(inRect);
         }
 
         public override string SettingsCategory()
